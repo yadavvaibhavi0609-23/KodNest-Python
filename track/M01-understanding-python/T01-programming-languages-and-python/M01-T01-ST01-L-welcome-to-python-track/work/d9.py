b0dc6680e00c7fecd31b08df9d9d17dcf819b0ea
Vaibhavi <yadavvaibhavi0609@gmail.com>
2026-08-07 12:41:23 +0530

    day3changes

---
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/d9.py
@@ -0,0 +1,12 @@
+age_text = "21"
+course_fee_text = "499.50"
+attempts = 3
+age = int(age_text)
+coursefee = float(course_fee_text)
+attempts = str(attempts)
+print(age)
+print(type(age))
+print(coursefee)
+print(type(coursefee))
+print(attempts)
+print(type(attempts))
