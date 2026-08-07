96a5b633a607e2fcfcfd6c7c1e899c6065b498e0
Vaibhavi <yadavvaibhavi0609@gmail.com>
2026-08-07 12:35:10 +0530

    day2changes

---
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/d8.py
@@ -0,0 +1,8 @@
+student_name = "Aarav"
+course_duration = 30
+course_rating = 4.8
+is_enrolled = True
+print(type(student_name))
+print(type(course_duration))
+print(type(course_rating))
+print(type(is_enrolled))
