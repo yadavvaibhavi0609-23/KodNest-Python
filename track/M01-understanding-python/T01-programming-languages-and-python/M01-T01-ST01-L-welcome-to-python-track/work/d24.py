615e8166f77c1cf5d1ae3ca107bd911c395f3f83
vaibhavi <yadavvaibhavi0609@gmail.com>
2026-08-12 00:45:19 +0530

    day6changes

---
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/d24.py
@@ -0,0 +1,9 @@
+name = input()
+course = input()
+score = int(input())
+student_recored = [name, course, score]
+student_name = ("Student_Name:",course)
+student_score = ("Studet_Score:",score)
+print(f"Name: {name}")
+print(f"Course: {course}")
+print(f"Score: {score}")
