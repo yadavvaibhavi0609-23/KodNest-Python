b5f7e74e68a6ded547e239deb37a10e35a99378d
vaibhavi <yadavvaibhavi0609@gmail.com>
2026-08-12 00:53:42 +0530

    day6changes

---
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/d25.py
@@ -0,0 +1,8 @@
+course_name = input()
+course_week = input()
+course_status = input()
+course_details = [course_name,current_week,course_status)
+updated_week = input()
+tup = (course_name,updated_week,course_status)
+course_details = tup
+print(course_details)
